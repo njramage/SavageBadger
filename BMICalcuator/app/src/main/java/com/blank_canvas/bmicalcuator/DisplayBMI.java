7ea16c7096a66e5e6b20cf6c663f08bce358ab44
NathanRamage <naramage94@gmail.com>
2016-10-28 15:09:53 +1000

    Added DisplayBMI activity

---
+++ BMICalcuator/app/src/main/java/com/blank_canvas/bmicalcuator/DisplayBMI.java
@@ -0,0 +1,16 @@
+package com.blank_canvas.bmicalcuator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nathan on 28/10/2016.
+ */
+
+public class DisplayBMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- BMICalcuator/app/src/main/java/com/blank_canvas/bmicalcuator/MainActivity.java
+++ BMICalcuator/app/src/main/java/com/blank_canvas/bmicalcuator/MainActivity.java
@@ -1,7 +1,11 @@
 package com.blank_canvas.bmicalcuator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit(View view) {
+        double bmi = calculateBMI(0,0,"Male");
+        Intent intent = new Intent(this, DisplayBMI.class);
+
+    }
+
+    private double calculateBMI(int height, int weight, String gender) {
+        return 0;
     }
 }
