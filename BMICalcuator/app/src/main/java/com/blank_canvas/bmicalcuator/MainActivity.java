f5b62881c9f7312b35d9b5f33454c6595c366a7f
NathanRamage <naramage94@gmail.com>
2016-10-28 15:55:35 +1000

    Created Display BMI layout
    Made intent to start Display bmi activity

--- BMICalcuator/app/src/main/java/com/blank_canvas/bmicalcuator/MainActivity.java
+++ BMICalcuator/app/src/main/java/com/blank_canvas/bmicalcuator/MainActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_GENDER = "Gender";
+    public final static String EXTRA_BMI = "BMI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submit(View view) {
-        double bmi = calculateBMI(0,0,"Male");
-        Intent intent = new Intent(this, DisplayBMI.class);
 
+        /* Get selected gender */
+        RadioGroup genderGroup = (RadioGroup) findViewById(R.id.gender_radiogroup);
+        int genderId = genderGroup.getCheckedRadioButtonId();
+        RadioButton genderRadio = (RadioButton) findViewById(genderId);
+        String gender = genderRadio.getText().toString();
+
+        /* Get entered height */
+        EditText heightText = (EditText) findViewById(R.id.height_textfield);
+        double height = Double.parseDouble(heightText.getText().toString());
+
+        /* Get entered weight */
+        EditText weightText = (EditText) findViewById(R.id.weight_textfield);
+        double weight = Double.parseDouble(heightText.getText().toString());
+
+        double bmi = calculateBMI(height, weight, gender); // calulate BMI
+
+        /* Start Display BMI activity and send the BMI and gender to it */
+        Intent intent = new Intent(this, DisplayBMI.class);
+        intent.putExtra("Gender", gender);
+        intent.putExtra("BMI", bmi);
+        startActivity(intent);
     }
 
-    private double calculateBMI(int height, int weight, String gender) {
+    private double calculateBMI(double height, double weight, String gender) {
         return 0;
     }
 }
