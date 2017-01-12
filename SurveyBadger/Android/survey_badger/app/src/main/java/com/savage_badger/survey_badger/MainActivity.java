f894aba68a81194fc2cd8a9b68545ae600d50c6a
NathanRamage <nramage94@gmail.com>
2017-01-12 13:37:34 +1000

    Added comments in CreateQuestions method for readability

--- SurveyBadger/Android/survey_badger/app/src/main/java/com/savage_badger/survey_badger/MainActivity.java
+++ SurveyBadger/Android/survey_badger/app/src/main/java/com/savage_badger/survey_badger/MainActivity.java
@@ -39,156 +39,40 @@ public class MainActivity extends AppCompatActivity {
 
         answersList = new ArrayList<Answer>();
 
-        jsonObject = null;//TODO: fetch JSON object with questions
-        createQuestions(jsonObject);
+        jsonObject = null;
+        createQuestions(jsonObject);/// convert JSON object into a list of question objects
     }
 
     // Creates a list of question objects
     public void createQuestions(JSONObject jsonObject) {
 
         try {
-
+            // turns Question JSON object into a JSON array for easy manipulation
             JSONArray questions = jsonObject.getJSONArray("questions");
 
             // fill questions list
-            for (int i = 0; i < questions.length(); i++) {
+            for (int i = 0; i < questions.length(); i++) { // iterate theough all the questions
                 JSONObject q = questions.getJSONObject(i);
 
+                // turns Answers JSON object into a JSON array for easy manipulation
                 JSONObject answersJSON = q.getJSONObject("answers");
                 try {
+                    // get all possible answers
                     JSONArray answers = answersJSON.getJSONArray("answers");
 
-                    List<String> possibleAnswers = new List<String>() {
-                        @Override
-                        public int size() {
-                            return 0;
-                        }
+                    // store them in an array for easy use
+                    ArrayList<String> possibleAnswers = new ArrayList<String>();
 
-                        @Override
-                        public boolean isEmpty() {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean contains(Object o) {
-                            return false;
-                        }
-
-                        @NonNull
-                        @Override
-                        public Iterator<String> iterator() {
-                            return null;
-                        }
-
-                        @NonNull
-                        @Override
-                        public Object[] toArray() {
-                            return new Object[0];
-                        }
-
-                        @NonNull
-                        @Override
-                        public <T> T[] toArray(T[] a) {
-                            return null;
-                        }
-
-                        @Override
-                        public boolean add(String s) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean remove(Object o) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean containsAll(Collection<?> c) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean addAll(Collection<? extends String> c) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean addAll(int index, Collection<? extends String> c) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean removeAll(Collection<?> c) {
-                            return false;
-                        }
-
-                        @Override
-                        public boolean retainAll(Collection<?> c) {
-                            return false;
-                        }
-
-                        @Override
-                        public void clear() {
-
-                        }
-
-                        @Override
-                        public String get(int index) {
-                            return null;
-                        }
-
-                        @Override
-                        public String set(int index, String element) {
-                            return null;
-                        }
-
-                        @Override
-                        public void add(int index, String element) {
-
-                        }
-
-                        @Override
-                        public String remove(int index) {
-                            return null;
-                        }
-
-                        @Override
-                        public int indexOf(Object o) {
-                            return 0;
-                        }
-
-                        @Override
-                        public int lastIndexOf(Object o) {
-                            return 0;
-                        }
-
-                        @Override
-                        public ListIterator<String> listIterator() {
-                            return null;
-                        }
-
-                        @NonNull
-                        @Override
-                        public ListIterator<String> listIterator(int index) {
-                            return null;
-                        }
-
-                        @NonNull
-                        @Override
-                        public List<String> subList(int fromIndex, int toIndex) {
-                            return null;
-                        }
-                    };
-
-                    // fill answers list
+                    // fill possible answers list
                     for (int j = 0; j < answers.length(); j++) {
                         String possibleAnswer = answers.getString(j);
                         possibleAnswers.add(possibleAnswer);
                     }
 
+                    // create an answers object
                     Question question = new Question(q.getInt("id"), q.getString("question"), q.getString("type"), possibleAnswers);
 
-                    questionsList.add(question);
+                    questionsList.add(question);// add it to the list
 
                 } catch (JSONException e) {
                     //TODO: Exception message
